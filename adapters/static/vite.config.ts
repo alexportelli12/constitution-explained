7ce8f84b5c3b0baeb845e83ddfe28a33359e435e
Alex Portelli <alexportelli12@gmail.com>
2025-07-23 09:34:28 +0200

    Update vite.config.ts

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://constitution-explained.web.app",
+        origin: "https://constitutionofmalta.com",
       }),
     ],
   };
